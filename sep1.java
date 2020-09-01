cbdcb8cdd68bfe631fa97317e2e170cc18133bf3
rohansahu1212 <rohansahu1212@gmail.com>
2020-09-01 10:54:15 +0530

    Create sep1.java

---
+++ sep1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class sep1{
+        public static void main(String[] args)
+        {
+        Scanner rs = new Scanner(System.in);
+        System.out.println("enter your name");
+        String name = rs.nextLine(); 
+        System.out.println("enter your age");
+        int age =rs.nextInt();    
+        System.out.println("hello, "+name+" and your age is "+age);
+        }
+}
